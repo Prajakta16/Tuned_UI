78edea1e3d6d989ea47cba9cdb57ee512722cb91
Aishwarya Chauhan <chauhan.ai@husky.neu.edu>
2020-04-19 22:44:31 -0400

    test5

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
-import {RequestOptions, Request, Headers } from '@angular/http';
 const hostName = "https://tuned-application.herokuapp.com"
 @Injectable({
   providedIn: 'root'
